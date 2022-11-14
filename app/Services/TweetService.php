c83465aee7939b8da2258c0d476e72037b71eec7
Kazumean <kazumin.angelav@gmail.com>
2022-11-14 17:46:36 +0900

    つぶやき投稿と同時に画像保存を行う処理の実装。

--- app/Services/TweetService.php
+++ app/Services/TweetService.php
@@ -4,6 +4,9 @@ namespace App\Services;
 
 use App\Models\Tweet;
 use Carbon\Carbon;
+use App\Models\Image;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class TweetService
 {
@@ -26,10 +29,30 @@ class TweetService
         return $tweet->user_id === $userId;
     }
 
+    // 前日のつぶやきの件数を取得する.
     public function countYesterdayTweets(): int
     {
         return Tweet::whereDate('created_at', '>=', Carbon::yesterday()->toDateTimeString())
             ->whereDate('created_at', '<', Carbon::today()->toDateTimeString())
             ->count();
+    }
+
+    // つぶやきと画像を一緒に保存する.
+    public function saveTweet(int $userId, string $content, array $images)
+    {
+        DB::transaction(function() use ($userId, $content, $images) {
+            $tweet = new Tweet;
+            $tweet->user_id = $userId;
+            $tweet->content = $content;
+            $tweet->save();
+
+            foreach($images as $image) {
+                Storage::putFile('public/images', $image);
+                $imageModel = new Image();
+                $imageModel->name = $image->hashName();
+                $imageModel->save();
+                $tweet->images()->attach($imageModel->id);
+            }
+        });
     }
 }
